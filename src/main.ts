521e1d99a6851f5d84a673bf4b271bb72785e36e
Brian Lu <lu_haoqin@outlook.com>
2021-04-17 01:38:54 -0500

    remove package.lock.json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as logging from "./logger";
 import * as models from "./models";
 import {Webserver} from './website';
+import {Users} from "./models";
 
 logging.setupLogging();
 models.syncModels();
